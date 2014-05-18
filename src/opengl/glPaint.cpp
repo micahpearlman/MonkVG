d60ac1544a8584a68fa5e59bfaccc8286c3895a1
Jorge Acereda <jacereda@gmail.com>
2014-05-18 23:11:36 +0200

    calcStops() should now be closer to the standard.

--- src/opengl/glPaint.cpp
+++ src/opengl/glPaint.cpp
@@ -44,27 +44,29 @@ namespace MonkVG {
 	}
 
 	void OpenGLPaint::calcStops(float ** stop0, float ** stop1, float g) {
+		assert(g >= 0);
+		assert(g <= 1);
 		size_t stopCnt = _colorRampStops.size();
 		float * s0 = 0;
 		float * s1 = 0;
-		for ( size_t i = 0; i < stopCnt; i++ ) {
-			if ( g >= _colorRampStops[i].a[0] ) {
-				s0 = _colorRampStops[i].a;
-				continue;
-			}
-			if ( s0 && g <= _colorRampStops[i].a[0] ) {
-				s1 = _colorRampStops[i].a;
-				break;
-			}
+		for ( size_t i = 0; i < stopCnt && !s1; i++ ) {
+			float * curr = _colorRampStops[i].a;
+			if ( g >= curr[0] )
+				s0 = curr;
+			else if ( s0 && g <= curr[0] )
+				s1 = curr;
 		}
-		if (!stopCnt) {
+		if ( stopCnt == 0 ) {
 			static float implicit0[] = {0,0,0,1};
 			static float implicit1[] = {1,1,1,1};
 			s0 = implicit0;
 			s1 = implicit1;
+		} else {
+			if ( !s0 )
+				s0 = _colorRampStops[0].a;
+			if ( !s1 )
+				s1 = _colorRampStops[stopCnt - 1].a;
 		}
-		if (!s1)
-			s1 = s0;
 		assert(s0[0] <= g && (g <= s1[0] || s1 == _colorRampStops[stopCnt-1].a));
 		*stop0 = s0;
 		*stop1 = s1;
