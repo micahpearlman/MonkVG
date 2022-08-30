00432991f6a11e3c4507d26c0fb12ea6e20ae500
Micah Pearlman <micah@zeroengineering.io>
2022-08-29 17:07:57 -0700

    added minimum glu library with cmake

---
+++ thirdparty/glu/autogen.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+autoreconf --force --install --verbose "$srcdir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
